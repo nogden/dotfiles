60f6a93116244f869368300e84fabbcc0cb6c9d0
Nick Ogden <nick.ogden@uswitch.com>
2016-09-20 10:54:13 +0100

    Only try to read AWS credentials if credentials file exists

--- customisations/shell-integration.el
+++ customisations/shell-integration.el
@@ -5,13 +5,16 @@
   (exec-path-from-shell-copy-envs
    '("PATH")))
 
-;; Set AWS environemnt variables for debugging within Emacs
 (defun read-lines (path)
   (with-temp-buffer
     (insert-file-contents path)
     (split-string (buffer-string) "\n" t)))
 
-(mapc '(lambda (line)
-         (let ((kv-pair (split-string line "=" t)))
-           (setenv (car kv-pair) (car (cdr kv-pair)))))
-      (cdr (read-lines "~/.aws/credentials")))
+(defvar aws-credentials-file "~/.aws/credentials")
+
+;; Set AWS environemnt variables for debugging within Emacs
+(when (file-readable-p aws-credentials-file)
+  (mapc '(lambda (line)
+           (let ((kv-pair (split-string line "=" t)))
+             (setenv (car kv-pair) (car (cdr kv-pair)))))
+        (cdr (read-lines aws-credentials-file))))
