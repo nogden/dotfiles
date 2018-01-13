644795e91b07a8a33a4f16941640763ee31c0cc9
Nick Ogden <nick@nickogden.org>
2018-01-13 21:33:07 +0000

    Add usual shortcuts for rust symbol navigation

--- .emacs.d/customisations/setup-rust.el
+++ .emacs.d/customisations/setup-rust.el
@@ -1,6 +1,3 @@
-;; Enable cargo mode in rust mode
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
-
 ;; Auto-insert closing brackets
 (add-hook 'rust-mode-hook 'electric-pair-mode)
 
@@ -12,3 +9,13 @@
 ;; Enable racer autocomplete
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+
+;; Usual navigation shortcuts
+(add-hook 'racer-mode-hook
+          (lambda ()
+            (local-set-key (kbd "<f2>") #'racer-find-definition)
+            (local-set-key (kbd "<f1>") #'pop-tag-mark)
+            (local-set-key (kbd "C-c C-d") #'racer-describe)))
+
+;; Enable cargo mode in rust mode
+(add-hook 'rust-mode-hook 'cargo-minor-mode)
