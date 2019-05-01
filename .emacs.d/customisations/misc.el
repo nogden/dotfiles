771da1d17c18d6ca0d91c97bc1ce58c7c0137289
Nick Ogden <nick@nickogden.org>
2019-05-01 20:41:40 +0100

    Ensure org-mode displays in clean mode

--- .emacs.d/customisations/misc.el
+++ .emacs.d/customisations/misc.el
@@ -1,3 +1,4 @@
 (use-package org
   :config
-  (setq org-default-notes-file (concat org-directory "/notes.org")))
+  (setq org-default-notes-file (concat org-directory "/notes.org")
+        org-startup-indented   t))
